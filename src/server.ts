b2256da00020ac092929f0dfd44775fb11c7232e
JeanCarlos <jmatochepascual@gmail.com>
2025-02-16 21:08:44 -0500

    feat: :wrench: Enable cors
    Configure the cors permissions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './data/dataBase';
 import projectRoutes from './routes/projectRoutes';
+import { CorsOptions } from 'cors';
 
 const app = express();
 
 // Connect to the database
 connectDB();
+
+// Enable CORS
+const corsList = [process.env.FRONTEMD_URL];
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || corsList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+app.use(cors(corsOptions));
 
 // Enable express.json
 app.use(express.json());
