dfe885d20e3bc55b845decd4d70c9d8cf7ae81cf
JeanCarlos <jmatochepascual@gmail.com>
2025-03-17 10:45:16 -0500

    feat: :recycle: Refactor middleware update password
    Implements the logic to update password validate

--- src/middlewares/userMiddleware.ts
+++ src/middlewares/userMiddleware.ts
@@ -99,6 +99,10 @@ class UserValidation {
     check('password')
       .notEmpty().withMessage('La contraseña es requerida')
       .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres'),
+
+    check('confirmPassword')
+      .notEmpty().withMessage('La confirmación de contraseña es requerida')
+      .custom((value, { req }) => value === req.body.password),
     (req: Request, res: Response, next: NextFunction) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
